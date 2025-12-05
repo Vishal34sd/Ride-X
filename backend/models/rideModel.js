82628c9016aa02ed89d5b0212ad6240f72c3bfda
Vishal <dubeyvishal714@gmail.com>
2025-12-05 19:38:41 +0530

    ride model created

---
+++ backend/models/rideModel.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+
+const rideSchema = new Mongoose({
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "user",
+        required : true
+    },
+    captain : {
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : "captain"
+    },
+    pickup : {
+        type : String ,
+        required : true ,
+
+    },
+    destination : {
+        type : String ,
+        required : true
+    },
+    fare : {
+        type : Number ,
+        required : true 
+    },
+    status : {
+        type : String ,
+        enum : ["pending" , "accepted" , "completed", "cancelled" , "ongoing"],
+        default : "pending"
+    },
+    duration : {
+        type : Number ,
+
+    },
+    distance : {
+        type : Number ,
+
+    },
+    paymentId : {
+        type : String
+    },
+    orderId : {
+        type : String
+    },
+    signature : {
+        type : String
+    },
+    otp : {
+        type : Number,
+        select : false,
+        required : true 
+    }
+}, {timestamps : true})
+
+const rideModel = mongoose.model("ride", rideSchema);
+export default rideModel ;
