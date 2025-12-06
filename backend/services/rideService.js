b8ddf0f885ebc3c3d1c24a7f132e9202d7b02a32
Vishal <dubeyvishal714@gmail.com>
2025-12-07 01:32:36 +0530

    new files added

--- backend/services/rideService.js
+++ backend/services/rideService.js
@@ -1,2 +1,29 @@
 import userModel from "../models/userModel.js";
 import rideModel from "../models/rideModel.js";
+import bcrypt from "bcrypt";
+import crypto from "crypto" ;
+
+
+export const getFareService = async(pickup , destination)=>{
+
+}
+
+const otp = (num)=>{
+
+}
+
+export const createRideService = async({user , pickup , destination , vehicleType})=>{
+
+}
+
+export const confirmRide = async({rideId , captain})=>{
+
+}
+
+export const startRide = async({rideId , otp , captain})=>{
+
+}
+
+export const endRide = async({rideId , captain})=>{
+    
+}
